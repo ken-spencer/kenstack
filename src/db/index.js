fb17987e9207ef57f2d7922ee375489ff1b5a29c
Kenneth Spencer <ken-spencer@users.noreply.github.com>
2024-05-13 15:17:20 -0700

    - removed unsed db connection file - database can now be reconnected to if it is down on initial connect

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,26 @@ import "server-only";
 // import { auditLog } from "logger";
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGO_URI, {
-  autoIndex: false, // Disable automatic index creation
-});
+
+function connect() {
+  return mongoose.connect(process.env.MONGO_URI, {
+    autoIndex: false, // Disable automatic index creation
+    maxPoolSize:50,
+  }).then(() => {
+  }, (e) => {
+    // Mongoose never connects if it fails initial connectionn. Wait 15 seconds and retry
+ 
+    // eslint-disable-next-line no-console
+    console.error("Failed initial database connection. Retrying in 15 seconds...");
+    setTimeout(() => {
+      connect();
+    }, 15000);
+  })
+}
+
+
+connect();
+
 
 mongoose.addModel = function (name, schema) {
   // hack to allow HMR to work.
