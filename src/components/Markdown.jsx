2c10b1522332d016b92c1b0baeba64940d14f229
Kenneth Spencer <ken-spencer@users.noreply.github.com>
2025-03-21 10:46:41 -0700

    Updated markdown configuration to allow for underlining and handle single newline

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -1,16 +1,37 @@
 import { remark } from "remark";
-import toHtml from "remark-html";
+// import toHtml from "remark-html";
 import remarkGfm from "remark-gfm";
+
+import remarkRehype from "remark-rehype";
+import rehypeSanitize from "rehype-sanitize";
+import rehypeStringify from "rehype-stringify";
+import { defaultSchema } from "hast-util-sanitize";
+import rehypeRaw from "rehype-raw";
+import remarkBreaks from "remark-breaks";
+
+const customSchema = {
+  ...defaultSchema,
+  tagNames: [...defaultSchema.tagNames, "u"],
+};
 
 export async function mdToHtml(content) {
   if (!content) {
     return "";
   }
 
+  const preprocessedContent = content.replace(
+    /__((?:(?!__).)+?)__/g,
+    "<u>$1</u>",
+  );
+
   const processedContent = await remark()
     .use(remarkGfm)
-    .use(toHtml)
-    .process(content);
+    .use(remarkBreaks) // Convert single newline to <br />    // .use(toHtml)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeSanitize, customSchema)
+    .use(rehypeStringify)
+    .process(preprocessedContent);
   return processedContent.toString();
 }
 
